e172a87a4d8d3ddc58d05604201707d1b545e195
Jordi Bertran de Balanda <jordi.bertran@scality.com>
2019-04-01 15:10:02 +0200

    buildchain: add dependency checking for Dockerfiles
    In order to track Dockerfile `COPY`/`ADD` image dependencies and
    to correctly rebuild images when their dependencies change, we require
    Dockerfile file dependencies to be tracked in the buildchain.
    
    The `LocalImage` target parses its Dockerfiles, checks the parsed dependencies
    against the local tree, and fails if dependencies are created in Dockerfiles
    but not reflected in target file dependencies.
    
    Issue: GH-885

--- buildchain/buildchain/targets/local_image.py
+++ buildchain/buildchain/targets/local_image.py
@@ -14,8 +14,11 @@ All of these actions are done by a single task.
 
 
 import operator
+import re
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from doit.exceptions import TaskError # type: ignore
 
 from buildchain import config
 from buildchain import constants
@@ -23,6 +26,20 @@ from buildchain import coreutils
 from buildchain import types
 
 from . import image
+
+
+def format_deps(
+        missing_deps: List[Tuple[int, str, str]],
+        filename: Path
+) -> str:
+    """Format error message for computed dependencies."""
+    err = 'Missing Dockerfile file dependencies in build target: \n{}:\n\t{}'
+    errors = [
+        'l.{}: {} missing {}'.format(pos, line, path)
+        for pos, line, path in missing_deps
+    ]
+    deps_msg = '\n\t'.join(errors)
+    return err.format(filename, deps_msg)
 
 
 class LocalImage(image.ContainerImage):
@@ -62,10 +79,61 @@ class LocalImage(image.ContainerImage):
             **kwargs
         )
 
-
     dockerfile   = property(operator.attrgetter('_dockerfile'))
     save_on_disk = property(operator.attrgetter('_save'))
     build_args   = property(operator.attrgetter('_build_args'))
+    dep_re = re.compile(
+        r'^\s*(COPY|ADD)( --[^ ]+)* (?P<src>[^ ]+) (?P<dst>[^ ]+)\s*$'
+    )
+
+    def load_deps_from_dockerfile(self) -> List[Tuple[int, str, str]]:
+        """Compute file dependencies from Dockerfile."""
+        dep_keywords = ('COPY', 'ADD')
+
+        with open(self.dockerfile, 'r') as dockerfile:
+            docker_lines = dockerfile.readlines()
+
+        dep_lines = [
+            (pos, line.strip(), self.dep_re.match(line.strip()))
+            for (pos, line) in enumerate(docker_lines, 1)
+            if line.strip().startswith(dep_keywords)
+        ]
+        deps = []
+        for (pos, line, match) in dep_lines:
+            if match is None:
+                continue
+            source = match.group('src')
+            path = self.dockerfile.parent/Path(source)
+            # Simple file
+            if path.is_file():
+                deps.append((pos, line, path))
+            # Directory or `.`
+            elif path.is_dir():
+                glob_deps = [
+                    (pos, line, sub_path)
+                    for sub_path in list(coreutils.ls_files_rec(path))
+                ]
+                deps += glob_deps
+            # Globs - `*` or specific e.g. `*.py`, `*.repo`
+            else:
+                glob_deps = [
+                    (pos, line, sub_path)
+                    for sub_path in path.parent.glob(path.name)
+                    if sub_path.is_file()
+                ]
+                deps += glob_deps
+        return deps
+
+    def check_dockerfile_dependencies(self) -> Union[TaskError, bool]:
+        """Verify task file dependencies against computed file dependencies."""
+        deps = self.load_deps_from_dockerfile()
+        missing_deps = []
+        for pos, line, path in deps:
+            if path not in self.file_dep:
+                missing_deps.append((pos, line, path))
+        if missing_deps:
+            return TaskError(msg=format_deps(missing_deps, self.dockerfile))
+        return True
 
     @property
     def task(self) -> types.TaskDict:
@@ -84,6 +152,10 @@ class LocalImage(image.ContainerImage):
 
     def _build_actions(self) -> List[types.Action]:
         """Build a container image locally."""
+        actions : List[types.Action] = []
+
+        actions.append(self.check_dockerfile_dependencies)
+
         build_cmd = [
             config.DOCKER, 'build',
             '--tag', self.tag,
@@ -92,8 +164,8 @@ class LocalImage(image.ContainerImage):
         for arg, value in self.build_args.items():
             build_cmd.extend(['--build-arg', '{}={}'.format(arg, value)])
         build_cmd.append(self.dockerfile.parent)
+        actions.append(build_cmd)
 
-        actions : List[types.Action] = [build_cmd]
         # If a destination is defined, let's save the image there.
         if self.save_on_disk:
             filepath = self.uncompressed_filename
